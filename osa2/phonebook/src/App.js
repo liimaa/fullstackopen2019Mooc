3bb2d3829ba32a6f27e963a26232e2407c2e1cc8
Eemeli <eemelizi1@outlook.com>
2019-08-21 14:03:47 +0300

    2.6: The Phonebook Step1

---
+++ osa2/phonebook/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNewPerson = (event) => {
+    event.preventDefault()
+    setPersons([...persons, {name: newName}])
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const listPersons = () =>
+    persons.map(person => <p key={person.name}>{person.name}</p>)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleNewPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {listPersons()}
+    </div>
+  )
+}
+
+export default App
