8bcbc31b208e3399f7e1e49abd2b3442b84a6970
Eemeli <eemelizi1@outlook.com>
2019-08-21 14:40:05 +0300

    2.11: The Phonebook Step6

--- osa2/phonebook/src/App.js
+++ osa2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import FilterForm from './components/FilterForm'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -14,6 +15,13 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNewPerson = (event) => {
     event.preventDefault()
