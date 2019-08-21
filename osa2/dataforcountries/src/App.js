bf0e1623986b929cedc76db0280b316bbae85897
Eemeli <eemelizi1@outlook.com>
2019-08-21 17:38:40 +0300

    2.14*: Data for countries, step3

--- osa2/dataforcountries/src/App.js
+++ osa2/dataforcountries/src/App.js
@@ -1,37 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ListContry = (country) => {
-
+const ListContry = ({country, weather, show, images}) => {
   const [toggle, setToggle] = useState(false)
 
-  var f = null;
-  if(country.images !== null) {
-    f = <img src={country.flag} width={100} height={'auto'} alt={"flag"} />
+  var flagsElem;
+  if(images !== null) {
+    flagsElem = <img src={country.flag} width={100} height={'auto'} alt={"flag"} />
   }
 
   const handleShow = () => setToggle(!toggle)
 
-  console.log(toggle);
-
-  if(toggle) {
+  const listContryValues = () => {
     try {
       let languages = country.languages.map(country => <li key={country.name}> {country.name} </li>)
-        return(
-          <div>
-            <p>{country.name}</p>
-            <p>capital {country.capital} </p>
-            <p>population {country.population} </p>
-    
-            <h3>languages </h3>
-            <ul>{languages} </ul>
-            {f}
-            <button onClick={handleShow}>hide</button>
-          </div>
-        )
-      } catch (error) {
-        console.log("list error", error);
-      }
+      return(
+        <div>
+          <p>{country.name}</p>
+          <p>capital {country.capital} </p>
+          <p>population {country.population} </p>
+  
+          <h3>languages </h3>
+          <ul>{languages} </ul>
+          {flagsElem}
+          <button onClick={handleShow}>hide</button>
+        </div>
+      )
+    } catch (error) {
+      //console.log("country error", error);
+    }
+  }
+
+  const listWeatherValues = () => {
+    try {
+      return(
+        <div>
+          <p> Weather in {weather.location.name} </p>
+          <p> Temperature {weather.current.temp_c} Celsius </p>
+          <img src={weather.current.condition.icon} width={100} height={'auto'} alt={"weather ico"} />
+          <p> wind {weather.current.wind_kph} kph direction {weather.current.wind_dir}</p>
+        </div>
+      )
+    } catch (error) {
+      //console.log("weather error", error);
+    }
+  }
+
+  if(show) {
+    return(
+      <div>
+        {listContryValues()}
+        {listWeatherValues()}
+      </div>
+    )
+  }
+
+  if(toggle) {
+    return(
+      listContryValues()
+    )
   }
 
   return(
@@ -40,17 +67,26 @@ const ListContry = (country) => {
       <button onClick={handleShow}>show</button>
     </p>
   )
+
 }
+
 
 const App = () => {
 
   const [countries, setCountries] = useState([])
-  const [searchCountry, setCountry] = useState('sw')
+  const [weather, setWeather] = useState([])
+  const [searchCountry, setCountry] = useState('finland')
 
   useEffect(() => {
     axios.get("https://restcountries.eu/rest/v2/all")
-      .then(response => setCountries(response.data))
+    .then(response => setCountries(response.data))
   }, [])
+
+  useEffect(() => {
+    axios.get(`https://api.apixu.com/v1/current.json?key=${"f9f5e69117484ab08d1155534190206"}&q=${'finland'}`)
+    .then(response => setWeather(response.data))
+  }, [])
+
 
   const handleCountryChange = (event) => {
     setCountry(event.target.value)
@@ -70,10 +106,11 @@ const App = () => {
         ? <p>Too many matches</p> 
         : searchCountries().length === 1 
         ? searchCountries().map(country => 
-          <ListContry key={country.name} {...country} />)
+          <ListContry key={country.name} country={country} weather={weather} show={true} />)
         : searchCountries().map(country => 
-          <ListContry key={country.name} {...country} images={null} />)
+          <ListContry key={country.name}  country={country} images={null} />)
       }
+
     </div>
   )
 }
