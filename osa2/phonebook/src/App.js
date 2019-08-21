3922f42a01e0b28687c6a160c73319eb96bab5f6
Eemeli <eemelizi1@outlook.com>
2019-08-21 14:06:07 +0300

    2.7: The Phonebook Step2

--- osa2/phonebook/src/App.js
+++ osa2/phonebook/src/App.js
@@ -8,6 +8,10 @@ const App = () => {
 
   const handleNewPerson = (event) => {
     event.preventDefault()
+    if(persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
     setPersons([...persons, {name: newName}])
   }
 
