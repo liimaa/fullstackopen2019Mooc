0aadc30272b01a50076d8c25a55aa95d72811f1b
Eemeli <eemelizi1@outlook.com>
2019-08-24 23:38:57 +0300

    4.4: helper functions and unit tests, step2

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return blogs ? 1 : undefined
 }
 
+const totalLikes = (blogs) => {
+  if(blogs.length > 1) {
+    return blogs.reduce((acc, obj) => {
+      return acc + obj.likes
+    }, 0)
+  } else if(blogs.length === 0) {
+    return 0
+  }
+  return blogs.likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
