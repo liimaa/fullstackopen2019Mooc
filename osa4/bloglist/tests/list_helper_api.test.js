7596fd62ea5f526c52fb29ce51c3ec28c8b11759
Eemeli <eemelizi1@outlook.com>
2019-08-25 00:39:51 +0300

    4.10: Blog list tests, step3

--- osa4/bloglist/tests/list_helper_api.test.js
+++ osa4/bloglist/tests/list_helper_api.test.js
@@ -45,4 +45,24 @@ describe('Check if blog object _id === id', () => {
   })
 })
 
+describe('Check if blog can be added && increments total of blogs', () => {
+  test('Succeeds with 201 creation & increments', async () => {
+    const newblog = {
+      title: 'Reverend Insanity',
+      author: 'Gu Zhen Ren',
+      url: 'https://www.webnovel.com/book/7996858406002505/Reverend-Insanity',
+      likes: 1280,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newblog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(initialBlogs.length + 1)
+  })
+})
+
 afterAll(() => mongoose.connection.close())
