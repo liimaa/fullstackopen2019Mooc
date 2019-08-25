0def07df69bb13d1b39bc79952575903604b0ba6
Eemeli <eemelizi1@outlook.com>
2019-08-25 22:17:13 +0300

    5.6* Blog list frontend, step6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = (blog) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = (blog) => {
+  const [richView, setRichView] = useState(false)
+
+  return (
+    richView
+    ?
+    <div className='blog-item rich' onClick={() => setRichView(false)}>
+      <div> {blog.title} {blog.author} </div>
+      <div> {blog.url} </div>
+      <div> likes {blog.likes} <button>like</button></div>
+      <div> added by {blog.user.name} </div>
+    </div>
+    :
+    <div className='blog-item' onClick={() => setRichView(true)}>
+      {blog.title} {blog.author}
+    </div>
+  )
+}
 
 export default Blog
